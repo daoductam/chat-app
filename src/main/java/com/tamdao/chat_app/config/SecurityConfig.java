3d70e5334ac7cbe4c3289bfab098dff77e7899e4
Dao Duc Tam <ddt@gmail.com>
2025-08-18 21:50:29 +0700

    change 2

--- src/main/java/com/tamdao/chat_app/config/SecurityConfig.java
+++ src/main/java/com/tamdao/chat_app/config/SecurityConfig.java
@@ -76,7 +76,7 @@ public class SecurityConfig {
 
         CorsConfiguration corsConfiguration = new CorsConfiguration();
 //        corsConfiguration.setAllowedOriginPatterns(Arrays.asList("*"));
-        corsConfiguration.setAllowedOrigins(Arrays.asList("https://chat-app-frontend-seven-bice.vercel.app/"));
+        corsConfiguration.setAllowedOrigins(Arrays.asList("*"));
 
         corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         corsConfiguration.setAllowedHeaders(Arrays.asList("*"));
